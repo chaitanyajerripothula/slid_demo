614f55458aa7e5affb9a4db955053102a2a8cb17
songhyeonju <shyunju7@naver.com>
2021-06-14 15:35:14 +0900

    Fix VideoDocument

--- slid-demo-app/src/vdocs/VideoDocument.js
+++ slid-demo-app/src/vdocs/VideoDocument.js
@@ -1,17 +1,17 @@
 import React from "react";
-import Split from "react-split";
+import SplitPane from "react-split-pane";
 import VideoPlayer from "../vdocs/VideoPlayer";
 import VideoDocumentEditor from "../vdocs/VideoDocumentEditor";
 
 const VideoDocument = () => {
   return (
     <div>
-      <Split sizes={[60, 40]} minSize={[(580, 330)]} expandToMin={false}
+      <SplitPane sizes={[60, 40]} minSize={[(580, 330)]} expandToMin={false}
              gutterAlign="center" snapOffset={30} dragInterval={1}
              direction="horizontal" cursor="col-resize">
         <VideoPlayer />
         <VideoDocumentEditor />
-      </Split>
+      </SplitPane>
     </div>
   );
 };
