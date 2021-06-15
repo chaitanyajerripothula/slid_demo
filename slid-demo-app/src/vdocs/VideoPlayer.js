afc8765ae5bda6cc2f700f4a21e089f480253360
bleuuue <bleuuue@gmail.com>
2021-06-15 17:58:38 +0900

    videoPlayer feature implementation

--- slid-demo-app/src/vdocs/VideoPlayer.js
+++ slid-demo-app/src/vdocs/VideoPlayer.js
@@ -1,7 +1,168 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import Dropdown from 'react-bootstrap/Dropdown'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
 
 const VideoPlayer = () => {
-  return <div>VideoPlayer</div>;
+  const [isPlaying, setIsPlaying] = useState();
+  const [videoState, setVideoState] = useState();
+  const [playbackSpeed, setPlaybackSpeed] = useState(1);
+  const [skipInterval, setSkipInterval] = useState(5);
+
+  const videoPlayerRef = useRef();
+  const videoPlaceholderRef = useRef()
+  const videoBackwardButtonRef = useRef();
+  const videoForwardButtonRef = useRef();
+
+  const toggleIsPlaying = () => {
+    if (isPlaying) {
+      videoPlayerRef.current.pause();
+      setIsPlaying(false);
+    }
+    else {
+      videoPlayerRef.current.play();
+      setIsPlaying(true);
+    }
+  }
+
+  const updateVideoTime = () => {
+    console.log("냐옹");
+    //let direction;
+
+    // if(direction == "forward") videoPlayerRef.current.currentTime += skipInterval;
+    // else videoPlayerRef.current.currentTime -= skipInterval;
+
+    videoPlayerRef.current.currentTime += skipInterval;
+  }
+
+  return (
+    <div>
+      <div>
+        <div ref={videoPlaceholderRef}>
+          <ReactPlayer
+            url="https://vimeo.com/1084537"
+            playing={isPlaying}
+            ref={videoPlayerRef}
+            onReady={() => {
+              videoPlayerRef.current.forcePlay = () => {
+                setIsPlaying(true);
+              };
+            }}
+            onPlay={() => {
+              setIsPlaying(true);
+            }}
+            onPause={() => {
+              setIsPlaying(false);
+            }}
+            onError={() => {
+              setVideoState("unavailable");
+            }}
+            controls={true}
+            playbackRate={playbackSpeed}
+            config={{
+              file: {
+                attributes: {
+                  oncontextmenu: (e) => e.preventDefault(),
+                  controlsList: "nodownload",
+                },},}} />
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <Dropdown drop={'right'}>
+            <Dropdown.Toggle variant={'white'}>
+              <span>Video Setting</span>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <div>
+                <span>동영상 설정</span>
+              </div>
+              <div>
+                <div>
+                  <span>구간 이동 간격</span>
+                  <select
+                    value={skipInterval}
+                    onChange={(event) => {
+                      setSkipInterval(parseInt(event.target.value));
+                    }}>
+                    <option value={5}>5s</option>
+                    <option value={10}>10s</option>
+                    <option value={30}>30s</option>
+                    <option value={60}>60s</option>
+                  </select>
+                </div>
+                <div>
+                  <span>재생속도</span>
+                  <select
+                    value={playbackSpeed}
+                    onChange={(event) => {
+                      setPlaybackSpeed(parseFloat(event.target.value))
+                    }}>
+                    <option value={0.5}>0.5x</option>
+                    <option value={0.75}>0.75x</option>
+                    <option value={1}>1x</option>
+                    <option value={1.25}>1.25x</option>
+                    <option value={1.5}>1.5x</option>
+                    <option value={1.75}>1.75x</option>
+                    <option value={2}>2x</option>
+                    <option value={2.25}>2.25x</option>
+                    <option value={2.5}>2.5x</option>
+                  </select>
+                </div>
+              </div>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <div>
+          <div>
+            <OverlayTrigger
+              placement={'top'}
+              overlay={
+                <Tooltip>
+                  {`${skipInterval}초 뒤로 (Alt + J)`}
+                </Tooltip>
+              }>
+              <button
+                ref={videoBackwardButtonRef}
+                onClick={updateVideoTime}>
+                  -{skipInterval}{" "}
+              </button>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              placement={'top'}
+              overlay={
+                <Tooltip>
+                  {{isPlaying} ? `일시정지 (Alt + K)` : `재생 (Alt + K)`}
+                </Tooltip>
+              }>
+              <button onClick={toggleIsPlaying}></button>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              placement={'top'}
+              overlay={
+                <Tooltip>
+                  {`${skipInterval}초 앞으로 (Alt + L)`}
+                </Tooltip>
+              }>
+              <button
+                ref={videoForwardButtonRef}
+                onClick={updateVideoTime}>
+                  +{skipInterval}
+              </button>
+            </OverlayTrigger>
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default VideoPlayer;
