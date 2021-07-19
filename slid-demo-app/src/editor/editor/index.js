b5d15561f623d88d30a804f49af27ecc80047bd9
bleuuue <bleuuue@gmail.com>
2021-07-19 13:59:30 +0900

    modal

--- slid-demo-app/src/editor/editor/index.js
+++ slid-demo-app/src/editor/editor/index.js
@@ -100,6 +100,87 @@ class Editor extends React.PureComponent {
     this.setState({ fontSize: size ? size : "small" });
   };
 
+  showMarkupWindow = () => {
+    Swal.fire({
+      target: document.getElementById("toast-container"),
+      title: "펜필기",
+      customClass: {
+        container: "position-absolute",
+      },
+      html:
+        `<div className={styles["markup-container"]}>
+          <div className={styles["markup-tool-container"]}>
+            <div className={styles["markup-type-container"]}>
+              <div className={"btn-group"} role="group">
+                <button type="button" className={"btn btn-light"}>
+                  <img className={styles["markup-type-image"]} alt={'slid cursor icon'} src={"../../../design/assets/slid_cursor_icon.png"}/>
+                </button>
+                <button type="button" className={"btn btn-light"}>
+                  <img className={styles["markup-type-image"]} alt={'slid pen icon'} src={"../../../design/assets/slid_pen_icon.png"}/>
+                </button>
+                <button type="button" className={"btn btn-light"}>
+                  <img className={styles["markup-type-image"]} alt={'slid highlighter icon'} src={"../../../design/assets/slid_highlighter_icon.png"}/>
+                </button>
+                <button type="button" className={"btn btn-light"}>
+                  <img className={styles["markup-type-image"]} alt={'slid text icon'} src={"../../../design/assets/slid_text_icon.png"}/>
+                </button>
+              </div>
+            </div>
+            <div className={styles["color-picker-container"]}>
+              <div className={"btn-group"} role="group">
+                <button type="button" className={"styles["markup-tool-container"] btn btn-light">
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle fa-w-16 ImageMarkup_color-picker-toggle__LnQLN" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" color="rgba(0,95,252,1)">
+                    <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className={styles["size-picker-container"]}>
+              <div className={"btn-group"} role="group">
+                <button type="button" className={"styles["size-small"] btn btn-light"}>
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path>
+                  </svg>
+                </button>
+                <button type="button" className={"styles["size-mideum"] btn btn-light"}>
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path>
+                  </svg>
+                </button>
+                <button type="button" className={"styles["size-large"] btn btn-light"}>
+                  <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles["canvas-container"]}>
+            <div className={styles["canvas"]}>
+              <canvas></canvas>
+            </div>
+          </div>
+          <div className={styles["markup-control-container"]}>
+            <div className={styles["history-container"]}>
+              <button className={"styles["markup-control-btn"] btn btn-light"}>
+                <img className={"markup-type-img"} alt={'backward button'} src={"../../../public/design/assets/slid_backward_icon.png"}/>
+              </button>
+              <button className={"styles["markup-control-btn"] btn btn-light"}>
+                <img className={"markup-type-img"} alt={'forward button'} src={"../../../public/design/assets/slid_forward_icon.png"}/>
+              </button>
+              <button className={"styles["markup-control-btn"] btn btn-light"}>
+                <img className={"markup-type-img"} alt={'reset button'} src={"../../../public/design/assets/slid_trash_icon.png"}/>
+              </button>
+            </div>
+          </div>
+        </div>`,
+      position: "center",
+      confirmButtonText: "저장",
+      confirmButtonColor: "#007bff",
+      heightAuto: false,
+    }).then(() => {});
+  }
+
   render() {
     let { fontSize, isSaving } = this.state;
     const { width, lang, isMacOs } = this.props;
@@ -107,44 +188,8 @@ class Editor extends React.PureComponent {
     EDITOR_JS_TOOLS.image.config = {
       lang: lang,
       onClickMarkup: () => {
-        Swal.fire({
-          target: document.getElementById("toast-container"),
-          title: "펜필기",
-          html: (
-            <div className={`${styles[`markup-container`]}`}>
-              <div className={`${styles[`markup-tool-container`]}`}>
-                <div className={`${styles[`markup-type-container`]}`}>
-                  <div className={`btn-group`} role="group">
-                    <button type="button" className={`btn btn-light`}>
-                      <img className={`${styles[`markup-type-image`]}`} alt={'slid cursor icon'} src={`../../../design/assets/slid_cursor_icon.png`}/>
-                    </button>
-                    <button type="button" className={`btn btn-light`}>
-                      <img className={`${styles[`markup-type-image`]}`} alt={'slid pen icon'} src={`../../../design/assets/slid_pen_icon.png`}/>
-                    </button>
-                    <button type="button" className={`btn btn-light`}>
-                      <img className={`${styles[`markup-type-image`]}`} alt={'slid highlighter icon'} src={`../../../design/assets/slid_highlighter_icon.png`}/>
-                    </button>
-                    <button type="button" className={`btn btn-light`}>
-                      <img className={`${styles[`markup-type-image`]}`} alt={'slid text icon'} src={`../../../design/assets/slid_text_icon.png`}/>
-                    </button>
-                  </div>
-                </div>
-                <div className={`${styles[`color-picker-container`]}`}>
-                  <div className={`btn-group`} role="group">
-                    <button type="button" className={`${styles[`markup-tool-container`]} btn btn-light`}>
-                      
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ),
-          position: "center",
-          confirmButtonText: "저장",
-          confirmButtonColor: "#007bff",
-          heightAuto: false,
-        }).then(() => {});
-      },
+        this.showMarkupWindow();
+      }
     }
 
     this.handleAddListener();
