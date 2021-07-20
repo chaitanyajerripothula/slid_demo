90f590c9737cae84250c4137069e4f4f083e2788
shyunju7 <shyunju7@naver.com>
2021-07-20 17:15:42 +0900

    Fix editorHeader

--- slid-demo-app/src/editor/editorHeader/index.js
+++ slid-demo-app/src/editor/editorHeader/index.js
@@ -3,10 +3,12 @@ import styles from "./editorHeader.module.css";
 import slidLogoImg from "../../design/assets/slid_logo_with_text.png";
 const EditorHeader = () => {
   const slidDownloadURL = "https://chrome.google.com/webstore/detail/video-screenshot-note-tak/cgajiilhmpfemmdihjnodpibaffakjhj";
-
+  const slidURL = "https://slid.cc/"
   return (
     <div className={`${styles[`editor-header-container`]}`}>
-      <img className={`${styles[`editor-header-slid-logo-icon`]}`} src={slidLogoImg} alt="slidLogoImage" />
+      <a href={slidURL}>
+        <img className={`${styles[`editor-header-slid-logo-icon`]}`} src={slidLogoImg} alt="slidLogoImage" />
+      </a>
       <span>
         <a className={`${styles[`editor-header-link`]}`} href={slidDownloadURL} target="_blank">
           슬리드 설치하러 가기{" "}
